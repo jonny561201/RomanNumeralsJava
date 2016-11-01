7471798d016e9e2e3438bf9fbc0b5b200ba9ea0c
Jon Graf <jongraf1@gmail.com>
2016-10-31 21:25:26 -0500

    refactored to cleanup test class

--- src/test/java/RomanNumeralTests.java
+++ src/test/java/RomanNumeralTests.java
@@ -7,42 +7,36 @@ public class RomanNumeralTests {
     @Test
     public void convertGivenOneShouldReturnI() {
         String actual = romanNumeral.convert(1);
-
         Assert.assertEquals("I", actual);
     }
 
     @Test
     public void convertGivenTwoShouldReturnII() {
         String actual = romanNumeral.convert(2);
-
         Assert.assertEquals("II", actual);
     }
 
     @Test
     public void convertGivenThreeShouldReturnIII() {
         String actual = romanNumeral.convert(3);
-
         Assert.assertEquals("III", actual);
     }
 
     @Test
     public void convertGivenFourShouldReturnIV() {
         String actual = romanNumeral.convert(4);
-
         Assert.assertEquals("IV", actual);
     }
 
     @Test
     public void convertGivenFiveShouldReturnV() {
         String actual = romanNumeral.convert(5);
-
         Assert.assertEquals("V", actual);
     }
 
     @Test
     public void convertGivenSixShouldReturnVI() {
         String actual = romanNumeral.convert(6);
-
         Assert.assertEquals("VI", actual);
     }
 }
